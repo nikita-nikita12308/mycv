3024d69fbcb54f72ad191a075d2c78daad752a1d
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-07-10 22:28:41 +0300

    Serialize feature

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -3,16 +3,17 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
 
+export function Serialize(dto: any) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
 export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any) {}
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-        // Run something before a request is handled
-        // by the request handler
-        console.log('im runnin before handler', context);
-
         return handler.handle().pipe(
             map((data: any) => {
-                // Run something before the response is sent out
-                console.log('Runinn something before the response is sent out', data)
+                return plainToClass(this.dto, data, {
+                    excludeExtraneousValues: true
+                })
             }),
         );
     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,9 +12,11 @@ import { Body,
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update.user.dto';
 import { UsersService } from './users.service'
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
@@ -23,7 +25,6 @@ export class UsersController {
         this.usersService.create(body.email, body.password)
     }
 
-    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         console.log('handler is runinn')
