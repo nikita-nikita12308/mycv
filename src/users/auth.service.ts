bf2ab5c1812fc53245a1af24f3146f43128b5597
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-07-13 22:04:40 +0300

    hash password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-    
+    constructor(private usersService: UsersService) {}
+
+    async signup(email: string, password: string) {
+        // 1) see if email is in use 
+        const users = await this.usersService.find(email)
+        if(users.length) throw new BadRequestException('This email in use, try another')
+
+        // 2) hash the users password 
+            // generate a salt
+        const salt = randomBytes(8).toString('hex'); 
+            
+            // hash the password and salt together
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+            // joined the hashed result and the salt together
+        const result = salt + '.' + hash.toString('hex');
+
+        // 3) create new user and save it 
+
+        // 4) return the user
+    }
+
+    signin() {}
 }
