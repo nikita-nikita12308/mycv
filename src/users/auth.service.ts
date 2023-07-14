9c7a80434b10a3ae6a8351c13c18ab6246ad78b4
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-07-14 23:12:36 +0300

    signin implementation

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -31,5 +31,19 @@ export class AuthService {
         return user;
     }
 
-    signin() {}
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
+        if(!user) {
+            throw new NotFoundException('User not found')
+        }
+        const [salt, hash] = user.password.split('.');
+
+        const providedHash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if( hash !== providedHash.toString('hex')) {
+            throw new BadRequestException('bad password');
+        } 
+        
+        return user;
+    }
 }
