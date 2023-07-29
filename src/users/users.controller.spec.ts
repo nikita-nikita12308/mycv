393453be4a3f5dd6d77acf938d793f30c01b552d
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-07-29 16:18:27 +0300

    day 1

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,8 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let faleAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
