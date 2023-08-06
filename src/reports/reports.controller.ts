8b09f9ad58731f5ef3e9c66484eba4d38a8af80e
nikita-nikita12308 <76599044+nikita-nikita12308@users.noreply.github.com>
2023-08-06 23:38:18 +0300

    Prepare to prod

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -16,7 +16,7 @@ export class ReportsController {
 
     @Get()
     getEstimate(@Query() query: GetEstimateDto) {
-        return console.log(query)
+        return this.reportsService.createEstimate(query);
     }
 
     @Post()
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,12 +4,29 @@ import { EntityNotFoundError, Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
+import { query } from 'express';
 
 @Injectable()
 export class ReportsService {
     constructor(
         @InjectRepository(Report) private repo: Repository<Report>
     ) {}
+    
+    createEstimate({ make, model, lng, lat, year, milage }: GetEstimateDto) {
+        return this.repo.createQueryBuilder()
+            .select('AVG(price)', 'price')
+            .where('make = :make', { make })
+            .andWhere('model = :model', { model })
+            .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+            .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+            .andWhere('year - :year BETWEEN -3 AND 3', { year })
+            .andWhere('approved is TRUE')
+            .orderBy('ABS(milage - :milage)', 'DESC')
+            .setParameters({ milage })
+            .limit(3)   
+            .getRawOne()
+    }
 
     create(reportDto: CreateReportDto, user: User) {
         const report = this.repo.create(reportDto);
@@ -26,4 +43,6 @@ export class ReportsService {
         report.approved = approved;
         return this.repo.save(report)
     }
+
+
 }
